9c0cf2c09d467228c852dc0d20a975dadf5e9083
kamil0826p-cyber <kamil0826p@gmail.com>
2026-07-26 20:09:44 +0200

    Register character currency service

--- src/modules/characters/character.module.ts
+++ src/modules/characters/character.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RealmModule } from '../realm/realm.module.js';
+import { CharacterCurrencyService } from './character-currency.service.js';
 import { CharacterService } from './character.service.js';
 
 @Module({
   imports: [RealmModule],
-  providers: [CharacterService],
-  exports: [CharacterService],
+  providers: [CharacterService, CharacterCurrencyService],
+  exports: [CharacterService, CharacterCurrencyService],
 })
 export class CharacterModule {}
