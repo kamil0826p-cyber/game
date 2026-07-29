5ba2bed3ec0e09e93eb9012b743d25d48bcb4fa7
kamil0826p-cyber <kamil0826p@gmail.com>
2026-07-29 18:48:58 +0200

    Connect PVE objectives to quest service

--- src/modules/mobs/mob.module.ts
+++ src/modules/mobs/mob.module.ts
@@ -3,6 +3,7 @@ import { KeyedSerialExecutor } from '../../common/utils/keyed-serial-executor.js
 import { MapModule } from '../maps/map.module.js';
 import { PersistenceModule } from '../persistence/persistence.module.js';
 import { TradeModule } from '../player/trade/trade.module.js';
+import { QuestModule } from '../quests/quest.module.js';
 import { SkillModule } from '../skills/skill.module.js';
 import { WorldModule } from '../world/world.module.js';
 import { MobCoordinatorService } from './mob-coordinator.service.js';
@@ -11,14 +12,8 @@ import { MobRewardService } from './mob-reward.service.js';
 import { PveCombatService } from './pve-combat.service.js';
 
 @Module({
-  imports: [MapModule, PersistenceModule, TradeModule, SkillModule, WorldModule],
-  providers: [
-    KeyedSerialExecutor,
-    MobRewardService,
-    MobCoordinatorService,
-    PveCombatService,
-    MobGateway,
-  ],
+  imports: [MapModule, PersistenceModule, TradeModule, QuestModule, SkillModule, WorldModule],
+  providers: [KeyedSerialExecutor, MobRewardService, MobCoordinatorService, PveCombatService, MobGateway],
   exports: [MobCoordinatorService, PveCombatService],
 })
 export class MobModule {}
