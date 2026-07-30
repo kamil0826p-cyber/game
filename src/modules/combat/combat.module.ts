79d0f50a54f150eafbdb315e38cf68749f2f9314
kamil0826p-cyber <kamil0826p@gmail.com>
2026-07-30 12:24:05 +0200

    Wire group and occupancy combat dependencies

--- src/modules/combat/combat.module.ts
+++ src/modules/combat/combat.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { KeyedSerialExecutor } from '../../common/utils/keyed-serial-executor.js';
+import { GroupModule } from '../groups/group.module.js';
 import { MapModule } from '../maps/map.module.js';
 import { MovementModule } from '../movement/movement.module.js';
 import { PersistenceModule } from '../persistence/persistence.module.js';
 import { TradeModule } from '../player/trade/trade.module.js';
 import { SkillModule } from '../skills/skill.module.js';
 import { WorldModule } from '../world/world.module.js';
+import { CombatOccupancyService } from './combat-occupancy.service.js';
 import { CombatGateway } from './combat.gateway.js';
 import { CombatService } from './combat.service.js';
 
 @Module({
-  imports: [MapModule, MovementModule, PersistenceModule, TradeModule, SkillModule, WorldModule],
-  providers: [KeyedSerialExecutor, CombatService, CombatGateway],
-  exports: [CombatService],
+  imports: [GroupModule, MapModule, MovementModule, PersistenceModule, TradeModule, SkillModule, WorldModule],
+  providers: [KeyedSerialExecutor, CombatOccupancyService, CombatService, CombatGateway],
+  exports: [CombatOccupancyService, CombatService],
 })
 export class CombatModule {}
