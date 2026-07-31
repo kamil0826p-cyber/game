8ada092ac4153e31d37a578c350df3aff6e684ca
kamil0826p-cyber <kamil0826p@gmail.com>
2026-07-31 19:03:02 +0200

    Provide progression to quest rewards (#74 #91 #92)

--- src/modules/quests/quest.module.ts
+++ src/modules/quests/quest.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CharacterModule } from '../characters/character.module.js';
 import { WorldModule } from '../world/world.module.js';
 import { QuestGateway } from './quest.gateway.js';
 import { QuestService } from './quest.service.js';
 
-@Module({ imports: [WorldModule], providers: [QuestService, QuestGateway], exports: [QuestService] })
+@Module({
+  imports: [CharacterModule, WorldModule],
+  providers: [QuestService, QuestGateway],
+  exports: [QuestService],
+})
 export class QuestModule {}
