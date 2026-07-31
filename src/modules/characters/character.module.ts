a776e09ac88c1d45dbc41f7a1772df46ad4c43f2
kamil0826p-cyber <kamil0826p@gmail.com>
2026-07-31 18:31:29 +0200

    Register progression service (#74)

--- src/modules/characters/character.module.ts
+++ src/modules/characters/character.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RealmModule } from '../realm/realm.module.js';
 import { CharacterCurrencyService } from './character-currency.service.js';
 import { CharacterService } from './character.service.js';
+import { ProgressionService } from './progression.service.js';
 
 @Module({
   imports: [RealmModule],
-  providers: [CharacterService, CharacterCurrencyService],
-  exports: [CharacterService, CharacterCurrencyService],
+  providers: [ProgressionService, CharacterService, CharacterCurrencyService],
+  exports: [ProgressionService, CharacterService, CharacterCurrencyService],
 })
 export class CharacterModule {}
