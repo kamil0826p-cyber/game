42f34c829e92309ec86b9f2bf5dd110d46d42927
kamil0826p-cyber <kamil0826p@gmail.com>
2026-08-01 15:03:13 +0200

    Use itemized canonical progression provider

--- src/modules/characters/character.module.ts
+++ src/modules/characters/character.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { RealmModule } from '../realm/realm.module.js';
 import { CharacterCurrencyService } from './character-currency.service.js';
 import { CharacterService } from './character.service.js';
+import { ItemizedCharacterProgressionService } from './progression/itemized-character-progression.service.js';
 import { CharacterProgressionService } from './progression/character-progression.service.js';
 
 @Module({
   imports: [RealmModule],
-  providers: [CharacterService, CharacterCurrencyService, CharacterProgressionService],
+  providers: [
+    CharacterService,
+    CharacterCurrencyService,
+    ItemizedCharacterProgressionService,
+    {
+      provide: CharacterProgressionService,
+      useExisting: ItemizedCharacterProgressionService,
+    },
+  ],
   exports: [CharacterService, CharacterCurrencyService, CharacterProgressionService],
 })
 export class CharacterModule {}
