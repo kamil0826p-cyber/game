5b582b03bc9f0e9d3674d04a360ac4bd0c813f87
Kamil Pacholczak <kamil897k@gmail.com>
2026-08-01 08:04:22 +0200

    fix prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
   datasource: {
     url: process.env.DIRECT_URL ?? process.env.DATABASE_URL ?? 'postgresql://game:game@localhost:5432/grid_mmorpg?schema=public',
   },
+  experimental: {
+    externalTables: true,
+  },
   tables: {
     external: [
       'public.ContentRelease',
