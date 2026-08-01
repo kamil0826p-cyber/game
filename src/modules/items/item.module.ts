1824d6d5a0d23e5163d837e4c4574cc0125ae6bc
kamil0826p-cyber <kamil0826p@gmail.com>
2026-08-01 10:04:06 +0200

    feat(progression): replace delta equipment service

--- src/modules/items/item.module.ts
+++ src/modules/items/item.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CharacterModule } from '../characters/character.module.js';
 import { MovementModule } from '../movement/movement.module.js';
 import { WorldModule } from '../world/world.module.js';
+import { CanonicalItemService } from './canonical-item.service.js';
 import { ItemGateway } from './item.gateway.js';
 import { ItemService } from './item.service.js';
 
 @Module({
-  imports: [WorldModule, MovementModule],
-  providers: [ItemService, ItemGateway],
+  imports: [WorldModule, MovementModule, CharacterModule],
+  providers: [
+    { provide: ItemService, useClass: CanonicalItemService },
+    ItemGateway,
+  ],
   exports: [ItemService],
 })
 export class ItemModule {}
