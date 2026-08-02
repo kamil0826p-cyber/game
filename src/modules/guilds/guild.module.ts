5417bdeada91fa40e5d0882834f0c11707c7f878
kamil0826p-cyber <kamil0826p@gmail.com>
2026-08-02 14:25:03 +0200

    Serialize guild economy commands with movement coordinator

--- src/modules/guilds/guild.module.ts
+++ src/modules/guilds/guild.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MovementModule } from '../movement/movement.module.js';
 import { WorldModule } from '../world/world.module.js';
 import { GuildGateway } from './guild.gateway.js';
 import { GuildService } from './guild.service.js';
 
 @Module({
-  imports: [WorldModule],
+  imports: [WorldModule, MovementModule],
   providers: [GuildService, GuildGateway],
   exports: [GuildService],
 })
