71b32e399c6f1a2cafbb6f18a6df3471750ca6a3
kamil0826p-cyber <kamil0826p@gmail.com>
2026-08-04 09:26:38 +0200

    Activate item curse combat hooks

--- src/modules/combat/combat.module.ts
+++ src/modules/combat/combat.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { KeyedSerialExecutor } from '../../common/utils/keyed-serial-executor.js';
 import { GroupModule } from '../groups/group.module.js';
+import { ItemModule } from '../items/item.module.js';
 import { MapModule } from '../maps/map.module.js';
 import { MovementModule } from '../movement/movement.module.js';
 import { NpcModule } from '../npcs/npc.module.js';
@@ -10,12 +11,14 @@ import { SkillModule } from '../skills/skill.module.js';
 import { WorldModule } from '../world/world.module.js';
 import { CombatOccupancyService } from './combat-occupancy.service.js';
 import { CombatGateway } from './combat.gateway.js';
+import './item-curse-combat.patch.js';
 import { CombatService } from './combat.service.js';
 import { DefeatRecoveryService } from './defeat-recovery.service.js';
 
 @Module({
   imports: [
     GroupModule,
+    ItemModule,
     MapModule,
     MovementModule,
     NpcModule,
