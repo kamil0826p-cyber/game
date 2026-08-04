a6d3b440f8e1f1fbe45ebc80cb4c8780f270a7ec
kamil0826p-cyber <kamil0826p@gmail.com>
2026-08-04 07:59:53 +0200

    Register defeat recovery in combat module

--- src/modules/combat/combat.module.ts
+++ src/modules/combat/combat.module.ts
@@ -3,6 +3,7 @@ import { KeyedSerialExecutor } from '../../common/utils/keyed-serial-executor.js
 import { GroupModule } from '../groups/group.module.js';
 import { MapModule } from '../maps/map.module.js';
 import { MovementModule } from '../movement/movement.module.js';
+import { NpcModule } from '../npcs/npc.module.js';
 import { PersistenceModule } from '../persistence/persistence.module.js';
 import { TradeModule } from '../player/trade/trade.module.js';
 import { SkillModule } from '../skills/skill.module.js';
@@ -10,10 +11,26 @@ import { WorldModule } from '../world/world.module.js';
 import { CombatOccupancyService } from './combat-occupancy.service.js';
 import { CombatGateway } from './combat.gateway.js';
 import { CombatService } from './combat.service.js';
+import { DefeatRecoveryService } from './defeat-recovery.service.js';
 
 @Module({
-  imports: [GroupModule, MapModule, MovementModule, PersistenceModule, TradeModule, SkillModule, WorldModule],
-  providers: [KeyedSerialExecutor, CombatOccupancyService, CombatService, CombatGateway],
+  imports: [
+    GroupModule,
+    MapModule,
+    MovementModule,
+    NpcModule,
+    PersistenceModule,
+    TradeModule,
+    SkillModule,
+    WorldModule,
+  ],
+  providers: [
+    KeyedSerialExecutor,
+    CombatOccupancyService,
+    CombatService,
+    DefeatRecoveryService,
+    CombatGateway,
+  ],
   exports: [CombatOccupancyService, CombatService],
 })
 export class CombatModule {}
